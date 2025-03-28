63471709da3aa5f5d75ea992c45447190cb18681
alia720 <68617399+alia720@users.noreply.github.com>
2025-03-28 04:39:11 -0600

    Update App.jsx

--- viteBReact/src/App.jsx
+++ viteBReact/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';  // Import React
 import Navbar from './Components/Navbar'; // Import Navbar component
-import './App.css'; // Optional: Import your styles
+import './App.css';
 import WaterRipple from './Components/WaterRipple';
 
 const App = () => {
